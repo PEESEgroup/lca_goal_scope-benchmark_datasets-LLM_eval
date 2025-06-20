32abebce6d2c275ad28c8e473a79e4479161509b
nathan-preuss <nnp25@cornell.edu>
2025-06-20 10:42:55 -0400

    ensure that missing data does not cause errors

--- data_cleaning.py
+++ data_cleaning.py
@@ -15,6 +15,7 @@ for entry_name in os.listdir(directory_path):
         data = json.load(f)
 
     # go through the list of available goal and scope tasks and find which ones are in Hestia
+    # TODO: check 10 other input files for differing categories that may be of interest
     # •	Intended application of results - not available in current data
     # •	Limitations due to methodological choices - not available in current data
     # •	Decision context and reasons for carrying out the study - not available in current data
@@ -22,41 +23,64 @@ for entry_name in os.listdir(directory_path):
     site = data['site']["@id"]
     with open("./data/recalculated/Site/{}".format(site)+extension, 'r') as f_site:
         data_site = json.load(f_site)
-        lca_data['siteDescription'] = data_site["description"]
-        lca_data['siteName'] = data_site["name"]
-        lca_data['siteType'] = data_site["siteType"]
-        lca_data['siteLocation'] = data_site["country"]["name"] + " - " + data_site["region"]["name"]
-    # •	Comparative studies to be disclosed to the public - some studies have comparative studies, which I think are called cycles
+        if "description" in data_site:
+            lca_data['siteDescription'] = data_site["description"]
+        if "name" in data_site:
+            lca_data['siteName'] = data_site["name"]
+        if "siteType" in data_site:
+            lca_data['siteType'] = data_site["siteType"]
+        if "country" in data_site:
+            if "region" in data_site:
+                lca_data['siteLocation'] = data_site["country"]["name"] + " - " + data_site["region"]["name"]
+            else:
+                lca_data['siteLocation'] = data_site["country"]["name"]
+
+    # •	Comparative studies to be disclosed to the public - some studies have comparative studies,
+    # which I think are called cycles
     cycle = data['cycle']["@id"]
     with open("./data/recalculated/Cycle/{}".format(cycle) + extension, 'r') as f_cycle:
         data_cycle = json.load(f_cycle)
-        lca_data['cycleDescription'] = data_cycle["description"]
-        lca_data['functionalUnit'] = data_cycle["functionalUnit"] #TODO: reconcile FU between here and data[product]
-        lca_data["systemBoundaryCompleteness"] = data_cycle["completeness"]
+        if "description" in data_cycle:
+            lca_data['cycleDescription'] = data_cycle["description"]
+        if "functionalUnit" in data_cycle:
+            lca_data['functionalUnit'] = data_cycle["functionalUnit"] #TODO: reconcile FU between here and data[product]
+        if "completeness" in data_cycle:
+            lca_data["systemBoundaryCompleteness"] = data_cycle["completeness"]
+
     # •	Commissioner of the study and other influential actors - source
     source = data['source']["@id"]
     with open("./data/Source/{}".format(source) + extension, 'r') as f_source:
         data_source = json.load(f_source)
-        lca_data['bibliography'] = data_source["bibliography"]
-        lca_data['notes'] = data_source["uploadNotes"]
+        if "bibliography" in data_source:
+            lca_data['bibliography'] = data_source["bibliography"]
+        if "uploadNotes" in data_source:
+            lca_data['notes'] = data_source["uploadNotes"]
     # •	Deliverables - not included in present data
     # •	Object of the assessment - name
-    lca_data['name'] = data['name']
+    if "name" in data:
+        lca_data['name'] = data['name']
     # •	LCI modelling framework and handling of multifunctional processes - allocationMethod, LCI modelling framework is
     # recalculated to include all possible LCIA methods (107 midpoint methods at a count)
-    lca_data['allocationMethod'] = data['allocationMethod']
+    if "allocationMethod" in data:
+        lca_data['allocationMethod'] = data['allocationMethod']
     # •	System boundaries and completeness requirements - functional unit quantity, product, info in cycle.json
-    lca_data['functionalUnitQuantity'] = data['functionalUnitQuantity']
-    lca_data["product_fate"] = data["product"]["fate"]
-    for i in data["product"]["properties"]:
-        lca_data["product{}properties".format(i)]["name"] = data["product"]["properties"][i]["term"]["name"]
-        lca_data["product{}properties".format(i)]["units"] = data["product"]["properties"][i]["term"]["units"]
-    lca_data["product_primary"] = data["product"]["primary"]
+    if "functionalUnitQuantity" in data:
+        lca_data['functionalUnitQuantity'] = data['functionalUnitQuantity']
+    if "product" in data:
+        if "fate" in data["product"]:
+            lca_data["product_fate"] = data["product"]["fate"]
+        if "properties" in data["product"]:
+            for i in range(len(data["product"]["properties"])):
+                lca_data["product{}properties".format(i)] = (data["product"]["properties"][i]["term"]["name"] + " - " +
+                                                             data["product"]["properties"][i]["term"]["units"])
+        if "primary" in data["product"]:
+            lca_data["product_primary"] = data["product"]["primary"]
     # •	Representativeness of LCI data - not available, but recalculated by Hestia
     # •	Preparation of the basis for impact assessment - not available in data
     # •	Special requirements for system comparisons - not available in data
     # •	Needs for critical review - not available in data
     # •	Planning reporting of results - not available in data, clearly results were reported
 
+    # write out data
     with open("data/cleaned/{}".format(entry_name), "w+") as w:
         json.dump(lca_data, w)
