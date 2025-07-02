efa3066e4a11aaac56c31183d4aef8f135c1ea95
nathan-preuss <nnp25@cornell.edu>
2025-07-02 10:38:32 -0400

    4 categories

--- qa_dataset_creation.py
+++ qa_dataset_creation.py
@@ -10,6 +10,7 @@ def intendedApplication(row):
                 "referenceResponse": [row["intendedApplication"]],
                 "category": "Intended Application"}
 
+
 def studyReasons(row):
     if row["studyReasons"].empty:
         return ""
@@ -17,6 +18,24 @@ def studyReasons(row):
         return {"prompt": "For this production system, " + row["systemDescription"] + ", what are the reasons for carrying out the LCA study?",
                 "referenceResponse": [row["studyReasons"]],
                 "category": "Study Reasons"}
+
+
+def targetAudience(row):
+    if row["intendedAudience"].empty:
+        return ""
+    else:
+        return {"prompt": "For this production system, " + row["systemDescription"] + ", what is the target audience of the LCA study?",
+                "referenceResponse": [row["intendedAudience"]],
+                "category": "Target Audience"}
+
+
+def comparativeAssertions(row):
+    if row["comparativeAssertions"].empty:
+        return ""
+    else:
+        return {"prompt": "For this production system, " + row["systemDescription"] + ", are these results to be used in comparative assertions?",
+                "referenceResponse": [row["comparativeAssertions"]],
+                "category": "Comparative Assertion"}
 
 def main(directory):
     # read in data
@@ -34,10 +53,13 @@ def main(directory):
     df["intendedApplicationQA"] =  df.apply(lambda row: intendedApplication(row), axis=1)
 
     # •	Limitations due to methodological choices - not available, skipping
-    df["studyReasonsQA"] = df.apply(lambda row: studyReasons(row), axis=1)
 
     # •	Decision context and reasons for carrying out the study
+    df["studyReasonsQA"] = df.apply(lambda row: studyReasons(row), axis=1)
+
     # •	Target audience
+    df["targetAudienceQA"] = df.apply(lambda row: targetAudience(row), axis=1)
+
     # •	Comparative studies to be disclosed to the public
     # •	Commissioner of the study and other influential actors
     # •	Deliverables
@@ -46,7 +68,10 @@ def main(directory):
     # •	System boundaries and completeness requirements
     # •	Representativeness of LCI data
     # •	Preparation of the basis for impact assessment
+
     # •	Special requirements for system comparisons
+    df["comparativeAssertionsQA"] = df.apply(lambda row: comparativeAssertions(row), axis=1)
+
     # •	Needs for critical review
     # •	Planning reporting of results
 
