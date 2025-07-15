8afe0ad2f5707b6fde29284c5ad61e1109959855
nathan-preuss <nnp25@cornell.edu>
2025-07-15 09:38:25 -0400

    get labels and preds

--- evaluate.py
+++ evaluate.py
@@ -2,26 +2,39 @@ import rag_retrieval
 import constants
 from langchain_community.vectorstores import FAISS
 import json
+import evaluate
 
 
-def evaluate(dataset, dataset_name, RAG=True):
+def eval_models(dataset, dataset_name, RAG=True):
     # for each model, we need to run an evaluation
     model_names = []
     for i in model_names:
         # configure model
         reader, tokenizer = rag_retrieval.model_config()
         print("model configured")
+        labels = []
+        preds = []
 
         # iterate through all questions in the dataset
         for question in dataset:
+            # extract question, label, and category
+            label = question["referenceResponse"]
+            category = question["category"]
+            prompt = question["prompt"]
+
             if RAG:
                 print(str(i) + " with RAG on dataset " + dataset_name)
-                answer, docs = rag_retrieval.answer_with_rag(question, reader, tokenizer, vdb)
+                answer, docs = rag_retrieval.answer_with_rag(prompt, reader, tokenizer, vdb)
             else:
                 print(str(i) + " without RAG on dataset " + dataset_name)
-                answer = rag_retrieval.answer_without_rag(question, reader, tokenizer)
+                answer = rag_retrieval.answer_without_rag(prompt, reader, tokenizer)
+
+            preds.append(answer)
+            labels.append(label)
 
         # compare answer to expected list of answers
+        metric = evaluate.load("accuracy")
+        metric.compute(predictions=preds, references=labels)
 
 
 
@@ -37,6 +50,6 @@ if __name__ == "__main__":
     for k in filenames:
         with open(k, 'r', encoding='utf-8') as f:
             data = json.load(f)
-            evaluate(data, k, True)
-            evaluate(data, k, False)
+            eval_models(data, k, True)
+            eval_models(data, k, False)
 
