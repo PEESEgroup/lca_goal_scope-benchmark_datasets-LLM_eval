5d136eaa4c7e7a1b9c5a32c6117dbf58445f18f4
nathan-preuss <nnp25@cornell.edu>
2025-07-15 10:31:09 -0400

    handle runtime errors with multiprocessing

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 import data_cleaning
 import make_data_table
 import qa_dataset_creation
+from multiprocessing import freeze_support
 
-data_cleaning.main("./data/")
-make_data_table.main("./data/")
-qa_dataset_creation.main("./data/", True)
-qa_dataset_creation.main("./data/", False)
 
-data_cleaning.main("./data/recalculated/")
-make_data_table.main("./data/recalculated/")
-qa_dataset_creation.main("./data/recalculated/", True)
-qa_dataset_creation.main("./data/recalculated/", False)
+if __name__ == '__main__':
+    freeze_support()
+    data_cleaning.main("./data/")
+    make_data_table.main("./data/")
+    qa_dataset_creation.main("./data/", True)
+    qa_dataset_creation.main("./data/", False)
+
+    data_cleaning.main("./data/recalculated/")
+    make_data_table.main("./data/recalculated/")
+    qa_dataset_creation.main("./data/recalculated/", True)
+    qa_dataset_creation.main("./data/recalculated/", False)
