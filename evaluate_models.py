5d38b2d3ec245c5e03a60a75847a93a0c54ebd01
nathan-preuss <nnp25@cornell.edu>
2025-09-05 11:19:25 -0400

    change to text classification scheme

--- evaluate_models.py
+++ evaluate_models.py
@@ -5,12 +5,14 @@ import evaluate
 import numpy as np
 from evaluate import evaluator
 from datasets import Dataset, load_dataset
-from transformers import AutoModelForSequenceClassification, TrainingArguments, Trainer, DataCollatorWithPadding, AutoTokenizer
+from transformers import AutoModelForSequenceClassification, TrainingArguments, Trainer, DataCollatorWithPadding, \
+    AutoTokenizer
 
 
 def preprocess_function(example, classes, class2id, tokenizer):
+    # TODO: replace with question and content from dataset files
     text = f"{example['title']}.\n{example['content']}"
-    all_labels = example['all_labels'].split(', ')
+    all_labels = example['all_labels']
     labels = [0. for i in range(len(classes))]
     for label in all_labels:
         label_id = class2id[label]
@@ -22,22 +24,24 @@ def preprocess_function(example, classes, class2id, tokenizer):
 
 
 def sigmoid(x):
-   return 1/(1 + np.exp(-x))
+    return 1 / (1 + np.exp(-x))
 
 
 def compute_metrics(eval_pred):
-
-   predictions, labels = eval_pred
-   predictions = sigmoid(predictions)
-   predictions = (predictions > 0.5).astype(int).reshape(-1)
-   clf_metrics = evaluate.combine(["accuracy", "f1", "precision", "recall"])
-   return clf_metrics.compute(predictions=predictions, references=labels.astype(int).reshape(-1))
+    predictions, labels = eval_pred
+    predictions = sigmoid(predictions)
+    predictions = (predictions > 0.5).astype(int).reshape(-1)
+    clf_metrics = evaluate.combine(["accuracy", "f1", "precision", "recall"])
+    return clf_metrics.compute(predictions=predictions, references=labels.astype(int).reshape(-1))
 
 
 def eval_models(dataset, dataset_name):
     # from: https://huggingface.co/blog/Valerii-Knowledgator/multi-label-classification
-    dataset = load_dataset('knowledgator/events_classification_biotech')
 
+    data = dataset
+    dataset = load_dataset('knowledgator/events_classification_biotech', trust_remote_code=True)
+
+    # a list of all of the unique classes
     classes = [class_ for class_ in dataset['train'].features['label 1'].names if class_]
     class2id = {class_: id for id, class_ in enumerate(classes)}
     id2class = {id: class_ for class_, id in class2id.items()}
@@ -45,22 +49,24 @@ def eval_models(dataset, dataset_name):
     model_path = 'microsoft/deberta-v3-small'
 
     tokenizer = AutoTokenizer.from_pretrained(model_path)
-    tokenized_dataset = dataset.map(preprocess_function)
+    tokenized_dataset = dataset.map(lambda example: preprocess_function(example, classes, class2id, tokenizer))
     data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
 
     model = AutoModelForSequenceClassification.from_pretrained(
-        model_path, num_labels=len(classes),
-        id2label=id2class, label2id=class2id,
+        model_path,
+        num_labels=len(classes),
+        id2label=id2class,
+        label2id=class2id,
         problem_type="multi_label_classification"
     )
     training_args = TrainingArguments(
-        output_dir="my_awesome_model",
+        output_dir="my_awesome_model"+dataset_name,
         learning_rate=2e-5,
         per_device_train_batch_size=3,
         per_device_eval_batch_size=3,
         num_train_epochs=2,
         weight_decay=0.01,
-        evaluation_strategy="epoch",
+        eval_strategy="epoch",
         save_strategy="epoch",
         load_best_model_at_end=True,
     )
@@ -71,72 +77,13 @@ def eval_models(dataset, dataset_name):
         train_dataset=tokenized_dataset["train"],
         eval_dataset=tokenized_dataset["test"],
         tokenizer=tokenizer,
-        data_collator = data_collator,
-        compute_metrics = compute_metrics,
+        data_collator=data_collator,
+        compute_metrics=compute_metrics,
     )
 
     trainer.train()
 
-    # for each model, we need to run an evaluation
-    model_names = ["distilbert-base-uncased-distilled-squad"]
-    for i in model_names:
-        # as label classification
-        print("evaluating dataset", dataset_name, " using model", i)
-        task_evaluator = evaluator("text-classification")
-        eval_results = task_evaluator.compute(
-            model_or_pipeline=i,
-            input_column="question",
-            label_column="answers",
-            label_mapping={},
-            data=dataset,
-            metric=evaluate.combine(["accuracy", "recall", "precision", "f1"])
-        )
-
-
-        """        # as qa
-        # TODO: filter by type???
-        task_evaluator = evaluator("question-answering")
-        print("evaluating dataset", dataset_name, " using model", i)
-        eval_results = task_evaluator.compute(
-            model_or_pipeline=i,
-            data=dataset,
-            metric="squad", #TODO: is the metric having difficulty due to the use of "squad". what others are available?
-            strategy="bootstrap",
-            n_resamples=30
-        )"""
-
-
-        """
-        # TODO: try text-generation
-        task_evaluator = evaluator("text-generation")
-        print("evaluating dataset", dataset_name, " using model", i)
-        eval_results = task_evaluator.compute(
-            model_or_pipeline=i,
-            data=dataset,
-            metric="squad",
-            strategy="bootstrap",
-            input_column="question",
-            label_column="answers",
-            n_resamples=30
-        )
-        """
-
-        """
-        # for text2text-generation, which may or may not be what we want
-        task_evaluator = evaluator("text2text-generation")
-        print("evaluating dataset", dataset_name, " using model", i)
-        eval_results = task_evaluator.compute(
-            model_or_pipeline=i,
-            data=dataset,
-            metric="squad",
-            strategy="bootstrap",
-            n_resamples=30
-        )
-
-        print(dataset_name, eval_results)
-        """
-
-        print(dataset_name, eval_results)
+    # TODO: evaluation
 
 
 if __name__ == "__main__":
@@ -146,8 +93,11 @@ if __name__ == "__main__":
         constants.VDB_LOCATION, embeddings, allow_dangerous_deserialization=True)
     print("vdb loaded")
 
-    # load qa dataset
-    filenames = ["data/recalculated/qa_dataset.jsonl", "data/qa_dataset.jsonl", "data/recalculated/rag_qa_dataset.jsonl", "data/rag_qa_dataset.jsonl"]
+    # load all datasets
+    filenames = ["data/recalculated/qa_dataset.jsonl", "data/qa_dataset.jsonl",
+                 "data/recalculated/rag_qa_dataset.jsonl", "data/rag_qa_dataset.jsonl"]
+
+    # for each dataset
     for k in filenames:
         data = []
         with open(k, 'r', encoding='utf-8') as f:
@@ -156,11 +106,8 @@ if __name__ == "__main__":
 
         # convert to dataset
         # TODO: combine question and context into a single entry
-        #TODO: rerun rag code
+        # TODO: rerun rag code
+        # TODO: test-train split
         dataset = Dataset.from_list(data)
-        data = load_dataset("squad", split="validation[:2]")
-        print(data[0])
-        print(dataset[0])
         print("dataset loaded")
         eval_models(dataset, k)
-
